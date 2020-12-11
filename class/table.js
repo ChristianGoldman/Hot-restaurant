db366e1d5f902f4833eb83842e5470a10902dfd5
James Leitschuh <james.leitschuh02@gmail.com>
2020-12-10 20:08:25 -0600

    Added TABLE class and linked to javascript file

---
+++ class/table.js
@@ -0,0 +1,10 @@
+class Table {
+    constructor(name,number,id,email){
+        this.name = name;
+        this.number = number;
+        this.id = id;
+        this.email = email;
+    };
+}
+
+module.exports = Table;
